c28d9e83c1fd3eea7cd8cf395d0c85676c9e7c60
Deepti Jain <deeptihnd000@gmail.com>
2024-09-16 18:54:27 +0530

    added navbar and routes

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import Home from "./Pages/Home/Home";
+// import Login from "./Pages/Auth/Login";
+// import SignUp from "./Pages/Auth/SignUp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import PrivateRoutes from "./Util/PrivateRoutes";
+// import Navbar from "./Components/Navbar/Navbar";
+// import DashboardPage from "./pages/dashboardPage/DashboardPage";
+import HomePage from "./pages/homePage/HomePage";
+import Navbar from "./components/navbar/Navbar";
+import LoginPage from "./pages/authPage/LoginPage";
+import SignUpPage from "./pages/authPage/SignUpPage";
 
+// Since App is a functional component, we can use the FC type from React
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        {/* <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          </Route> */}
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- web-app/src/components/navbar/Navbar.tsx
+++ web-app/src/components/navbar/Navbar.tsx
@@ -1,7 +1,95 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { useEffect, useState } from "react";
 
-const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+import "./Navbar.css";
+
+interface User {
+  username?: string;
+  email?: string;
+}
+
+const Header = () => {
+  const [user, setUser] = useState<User | null>(
+    JSON.parse(localStorage.getItem("user") || "null")
+  );
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "/login";
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    } else {
+      setUser(null);
+    }
+  }, [localStorage.getItem("user")]);
+
+  return (
+    <>
+      <Navbar
+        key="md"
+        expand="md"
+        className="bg-body-danger"
+        style={{
+          background:
+            "linear-gradient(to bottom, rgb(0, 123, 255) 0%, rgba(0, 123, 255,0.9) 33%, rgba(0, 123, 255,0.8) 50%, rgba(0, 123, 255, 0.7) 80%, rgba(255, 255, 255, 0) 100%)",
+        }}
+      >
+        <Container fluid>
+          <Navbar.Brand href="/">RED ALERT</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-md`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+            placement="end"
+          >
+            <Offcanvas.Header
+              closeButton
+              style={{
+                background: "rgb(0, 123, 255)",
+              }}
+            >
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+                RED ALERT
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className="offcanvas-bg-md-class">
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                {user ? (
+                  <>
+                    <Nav.Link as={Link} to="/logout">
+                      <button
+                        className="btn btn-primary my-0"
+                        onClick={handleLogout}
+                      >
+                        Logout
+                      </button>
+                    </Nav.Link>
+                  </>
+                ) : (
+                  <>
+                    <Nav.Link as={Link} to="/signUp">
+                      Sign Up
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/login">
+                      Login
+                    </Nav.Link>
+                  </>
+                )}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
-export default Navbar;
+export default Header;
